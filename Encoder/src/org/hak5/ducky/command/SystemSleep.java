002a88cdad27e15b63c6e5806aa7b169f434c1c8
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 16:59:05 -0400

    Implemented SYSTEMSLEEP command.
    SYSTEMSLEEP command is now represented by SystemSleep class.

---
+++ Encoder/src/org/hak5/ducky/command/SystemSleep.java
@@ -0,0 +1,37 @@
+package org.hak5.ducky.command;
+
+import org.hak5.ducky.State;
+import org.hak5.ducky.Command;
+
+/**
+ * @author Kevin Polulak
+ *
+ * Implementation of the <code>SYSTEMSLEEP</code> command. Used to put the
+ * computer to sleep.
+ */
+public class SystemSleep implements Command {
+    public final java.lang.String name    = "SYSTEMSLEEP";
+    public final java.lang.String altName = "";
+    public final java.lang.String help    = name;
+
+    @Override
+    public void action(State state, java.lang.String args) {
+        state.addByteToFile(0x82);
+        state.addByteToFile(0x00);
+    }
+    
+    @Override
+    public java.lang.String getName() {
+        return name;
+    }
+    
+    @Override
+    public java.lang.String getAltName() {
+        return altName;
+    }
+    
+    @Override
+    public java.lang.String getHelp() {
+        return help;
+    }
+}
