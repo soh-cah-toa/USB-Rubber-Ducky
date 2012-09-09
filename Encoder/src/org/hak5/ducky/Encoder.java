366bf3cd7d8cc3c87180f3b936bc3ce278eae450
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 15:15:23 -0400

    Fixed remaining whitespaces issues.
    Encoder.java and State.java were the other two files containing a mix of
    both hard and soft tabs. I think this is the last of them.

--- Encoder/src/org/hak5/ducky/Encoder.java
+++ Encoder/src/org/hak5/ducky/Encoder.java
@@ -20,127 +20,127 @@ import javax.swing.text.rtf.RTFEditorKit;
  */
 public class Encoder {
 	public static void main(String[] args) {
-		String inputFile  = null;
-		String outputFile = null;
+	    String inputFile  = null;
+	    String outputFile = null;
 
         // Display help message by default without arguments
-		if (args.length == 0) {
-            displayHelp();
-			System.exit(1);
-		}
+	    if (args.length == 0) {
+	        displayHelp();
+	        System.exit(1);
+	    }
 
         // TODO Consider using Scanner class to parse arguments
 
         // Process command-line switches
-		for (int i = 0; i < args.length; i++) {
-            // XXX This --gui switch seems to do nothing. Is it needed?
-			if (args[i].equals("--gui") || args[i].equals("-g"))
-				System.out.println("Launch GUI");
-			else if (args[i].equals("--help") || args[i].equals("-h"))
-                displayHelp();
-			else if (args[i].equals("-i"))
-				inputFile = args[++i];
-			else if (args[i].equals("-o"))
-				outputFile = args[++i];
-			else {
-                displayHelp();
-				break;
-			}
-		}
+	    for (int i = 0; i < args.length; i++) {
+	        // XXX This --gui switch seems to do nothing. Is it needed?
+	        if (args[i].equals("--gui") || args[i].equals("-g"))
+	            System.out.println("Launch GUI");
+	        else if (args[i].equals("--help") || args[i].equals("-h"))
+	            displayHelp();
+	        else if (args[i].equals("-i"))
+	            inputFile = args[++i];
+	        else if (args[i].equals("-o"))
+	            outputFile = args[++i];
+	        else {
+	            displayHelp();
+	            break;
+	        }
+	    }
 
         // Read input DuckyScript file
-		if (inputFile != null) {
-			String fileText = null;
+	    if (inputFile != null) {
+	        String fileText = null;
 
-			if (inputFile.contains(".rtf")) {
-				try {
-					FileInputStream stream = new FileInputStream(inputFile);
-					RTFEditorKit kit = new RTFEditorKit();
-					Document doc = kit.createDefaultDocument();
+	        if (inputFile.contains(".rtf")) {
+	            try {
+	                FileInputStream stream = new FileInputStream(inputFile);
+	                RTFEditorKit kit = new RTFEditorKit();
+	                Document doc = kit.createDefaultDocument();
 
-					kit.read(stream, doc, 0);
-					fileText = doc.getText(0, doc.getLength());
-				} catch (IOException e) {
-                    String errMsg = "[ERROR] Failed to read RTF file: "
-                                  + inputFile;
+	                kit.read(stream, doc, 0);
+	                fileText = doc.getText(0, doc.getLength());
+	            } catch (IOException e) {
+	                String errMsg = "[ERROR] Failed to read RTF file: "
+	                              + inputFile;
 
-					System.err.println(errMsg);
-				} catch (BadLocationException e) {
-                    String errMsg = "[ERROR] Failed to read from invalid "
-                                  + "location within file: "
-                                  + inputFile;
+	                System.err.println(errMsg);
+	            } catch (BadLocationException e) {
+	                String errMsg = "[ERROR] Failed to read from invalid "
+	                              + "location within file: "
+	                              + inputFile;
 
-					System.err.println(errMsg);
-				}
-			} else {
-				DataInputStream in = null;
+	                System.err.println(errMsg);
+	            }
+	        } else {
+	            DataInputStream in = null;
 
-				try {
-					File f = new File(inputFile);
-					byte[] buffer = new byte[(int) f.length()];
+	            try {
+	                File f = new File(inputFile);
+	                byte[] buffer = new byte[(int) f.length()];
 
-					in = new DataInputStream(new FileInputStream(f));
-					in.readFully(buffer);
+	                in = new DataInputStream(new FileInputStream(f));
+	                in.readFully(buffer);
 
-					fileText = new String(buffer);
-				} catch (IOException e) {
-                    String errMsg = "[ERROR] Failed to read text file: "
-                                  + inputFile;
+	                fileText = new String(buffer);
+	            } catch (IOException e) {
+	                String errMsg = "[ERROR] Failed to read text file: "
+	                              + inputFile;
 
-					System.err.println(errMsg);
-				} finally {
-					try {
-						in.close();
-					} catch (IOException e) { // Ignore exception
-					}
-				}
-			}
+	                System.err.println(errMsg);
+	            } finally {
+	                try {
+	                    in.close();
+	                } catch (IOException e) { // Ignore exception
+	                }
+	            }
+	        }
 
-            State state = State.newInstance(fileText,
-                                           (outputFile == null) ? "inject.bin"
-					                       : outputFile);
+	        State state = State.newInstance(fileText,
+	                                       (outputFile == null) ? "inject.bin"
+	                                       : outputFile);
 
-            state.begin();
-		}
+	        state.begin();
+	    }
 	}
 
     public static void displayHelp() {
-    	// TODO Re-implement this to use help messages associated with soon
-    	//      to come Command objects.
-		String message = "Hak5 Duck Encoder 2.0\n\n"
-				+ "Usage: duckencoder -i [file ..]\t\t\tencode specified file\n"
-				+ "   or: duckencoder -i [file ..] -o [file ..]\tencode to specified file\n"
-				+ "\nArguments:\n"
-				+ "   -i [file] \t\tInput DuckyScript file\n"
-				+ "   -o [file] \t\tOutput file\n"
-				+ "\nScript Commands:\n"
-				+ "   ALT [END | (ESCAPE | ESC) | F1...F12 | Single Char | SPACE | TAB]\n"
-				+ "   BREAK | PAUSE\n"
-				+ "   CAPSLOCK\n"
-				+ "   CONTROL | CTRL [(BREAK | PAUSE) | F1...F12 | (ESCAPE | ESC) | Single Char]\n"
-				+ "   DEFAULT_DELAY | DEFAULTDELAY [Time in millisecond * 10]\n"
-				+ "   DELAY [Time in millisecond * 10]\n"
-				+ "   DELETE\n"
-				+ "   DOWNARROW | DOWN\n"
-				+ "   END\n"
-				+ "   ESCAPE | ESC\n"
-				+ "   F1...F12\n"
-				+ "   HOME\n"
-				+ "   INSERT\n"
-				+ "   LEFTARROW | LEFT\n"
-				+ "   MENU | APP\n"
-				+ "   NUMLOCK\n"
-				+ "   PAGEDOWN\n"
-				+ "   PAGEUP\n"
-				+ "   PRINTSCREEN\n"
-				+ "   REM\n"
-				+ "   RIGHTARROW | RIGHT\n"
-				+ "   SCROLLLOCK\n"
-				+ "   SHIFT [ DELETE | HOME | INSERT | PAGEUP | PAGEDOWN | (WINDOWS | GUI)\n"
-				+ "         | (UPARROW | DOWNARROW |LEFTARROW | RIGHTARROW) | TAB]\n"
-				+ "   SPACE\n"
-				+ "   STRING [a...z A...Z 0..9 !...) `~ += _- \"\' :; <, >. ?/ \\|]\n"
-				+ "   TAB\n" + "   UPARROW | UP\n" + "   WINDOWS | GUI\n";
+        // TODO Re-implement this to use help messages associated with soon
+        //      to come Command objects.
+        String message = "Hak5 Duck Encoder 2.0\n\n"
+            + "Usage: duckencoder -i [file ..]\t\t\tencode specified file\n"
+            + "   or: duckencoder -i [file ..] -o [file ..]\tencode to specified file\n"
+            + "\nArguments:\n"
+            + "   -i [file] \t\tInput DuckyScript file\n"
+            + "   -o [file] \t\tOutput file\n"
+            + "\nScript Commands:\n"
+            + "   ALT [END | (ESCAPE | ESC) | F1...F12 | Single Char | SPACE | TAB]\n"
+            + "   BREAK | PAUSE\n"
+            + "   CAPSLOCK\n"
+            + "   CONTROL | CTRL [(BREAK | PAUSE) | F1...F12 | (ESCAPE | ESC) | Single Char]\n"
+            + "   DEFAULT_DELAY | DEFAULTDELAY [Time in millisecond * 10]\n"
+            + "   DELAY [Time in millisecond * 10]\n"
+            + "   DELETE\n"
+            + "   DOWNARROW | DOWN\n"
+            + "   END\n"
+            + "   ESCAPE | ESC\n"
+            + "   F1...F12\n"
+            + "   HOME\n"
+            + "   INSERT\n"
+            + "   LEFTARROW | LEFT\n"
+            + "   MENU | APP\n"
+            + "   NUMLOCK\n"
+            + "   PAGEDOWN\n"
+            + "   PAGEUP\n"
+            + "   PRINTSCREEN\n"
+            + "   REM\n"
+            + "   RIGHTARROW | RIGHT\n"
+            + "   SCROLLLOCK\n"
+            + "   SHIFT [ DELETE | HOME | INSERT | PAGEUP | PAGEDOWN | (WINDOWS | GUI)\n"
+            + "         | (UPARROW | DOWNARROW |LEFTARROW | RIGHTARROW) | TAB]\n"
+            + "   SPACE\n"
+            + "   STRING [a...z A...Z 0..9 !...) `~ += _- \"\' :; <, >. ?/ \\|]\n"
+            + "   TAB\n" + "   UPARROW | UP\n" + "   WINDOWS | GUI\n";
 
         System.out.println(message);
     }
--- Encoder/src/org/hak5/ducky/State.java
+++ Encoder/src/org/hak5/ducky/State.java
@@ -130,124 +130,124 @@ public class State {
 	public void addCharToFile(char c) {
         byte value = (byte) 0x99;
 
-		if ((int) c >= 97 && (int) c <= 122)     // Upper-case letters
-			value = (byte) (c - 0x5D);
-		else if ((int) c >= 65 && (int) c <= 90) // Lower-case letters
-			value = (byte) (c - 0x3D);
-		else if ((int) c >= 49 && (int) c <= 57) // Digits 0-9
-			value = (byte) (c - 0x13);
-		else                                     // Punctuation
-			switch (c) {
-			case ' ':
-				value = (byte) 0x2C;
-			case '!':
-				value = (byte) 0x1e;
-			case '@':
-				value = (byte) 0x1f;
-			case '#':
-				value = (byte) 0x20;
-			case '$':
-				value = (byte) 0x21;
-			case '%':
-				value = (byte) 0x22;
-			case '^':
-				value = (byte) 0x23;
-			case '&':
-				value = (byte) 0x24;
-			case '*':
-				value = (byte) 0x25;
-			case '(':
-				value = (byte) 0x26;
-			case ')':
-			case '0':
-				value = (byte) 0x27;
-			case '-':
-			case '_':
-				value = (byte) 0x2D;
-			case '=':
-			case '+':
-				value = (byte) 0x2E;
-			case '[':
-			case '{':
-				value = (byte) 0x2F;
-			case ']':
-			case '}':
-				value = (byte) 0x30;
-			case '\\':
-			case '|':
-				value = (byte) 0x31;
-			case ':':
-			case ';':
-				value = (byte) 0x33;
-			case '\'':
-			case '"':
-				value = (byte) 0x34;
-			case '`':
-			case '~':
-				value = (byte) 0x35;
-			case ',':
-			case '<':
-				value = (byte) 0x36;
-			case '.':
-			case '>':
-				value = (byte) 0x37;
-			case '/':
-			case '?':
-				value = (byte) 0x38;
-			}
+        if ((int) c >= 97 && (int) c <= 122)     // Upper-case letters
+            value = (byte) (c - 0x5D);
+        else if ((int) c >= 65 && (int) c <= 90) // Lower-case letters
+            value = (byte) (c - 0x3D);
+        else if ((int) c >= 49 && (int) c <= 57) // Digits 0-9
+            value = (byte) (c - 0x13);
+        else                                     // Punctuation
+            switch (c) {
+            case ' ':
+                value = (byte) 0x2C;
+            case '!':
+                value = (byte) 0x1e;
+            case '@':
+                value = (byte) 0x1f;
+            case '#':
+                value = (byte) 0x20;
+            case '$':
+                value = (byte) 0x21;
+            case '%':
+                value = (byte) 0x22;
+            case '^':
+                value = (byte) 0x23;
+            case '&':
+                value = (byte) 0x24;
+            case '*':
+                value = (byte) 0x25;
+            case '(':
+                value = (byte) 0x26;
+            case ')':
+            case '0':
+                value = (byte) 0x27;
+            case '-':
+            case '_':
+                value = (byte) 0x2D;
+            case '=':
+            case '+':
+                value = (byte) 0x2E;
+            case '[':
+            case '{':
+                value = (byte) 0x2F;
+            case ']':
+            case '}':
+                value = (byte) 0x30;
+            case '\\':
+            case '|':
+                value = (byte) 0x31;
+            case ':':
+            case ';':
+                value = (byte) 0x33;
+            case '\'':
+            case '"':
+                value = (byte) 0x34;
+            case '`':
+            case '~':
+                value = (byte) 0x35;
+            case ',':
+            case '<':
+                value = (byte) 0x36;
+            case '.':
+            case '>':
+                value = (byte) 0x37;
+            case '/':
+            case '?':
+                value = (byte) 0x38;
+            }
 
         addByteToFile(value);
-	}
+    }
 
 	/**
 	 * Writes function key value to output file.
 	 * 
 	 * @param fKey string to write
 	 */
-	public void addFunctionKeyToFile(String fKey) {
+    public void addFunctionKeyToFile(String fKey) {
         byte value = (byte) 0x99;
 
         switch (fKey.toUpperCase()) {
-		case "F1":
-			value = (byte) 0x3a;
-			break;
-		case "F2":
-			value = (byte) 0x3b;
-			break;
-		case "F3":
-			value = (byte) 0x3c;
-			break;
-		case "F4":
-			value = (byte) 0x3d;
-			break;
-		case "F5":
-			value = (byte) 0x3e;
-			break;
-		case "F6":
-			value = (byte) 0x3f;
-			break;
-		case "F7":
-			value = (byte) 0x40;
-			break;
-		case "F8":
-			value = (byte) 0x41;
-			break;
-		case "F9":
-			value = (byte) 0x42;
-			break;
-		case "F10":
-			value = (byte) 0x43;
-			break;
-		case "F11":
-			value = (byte) 0x44;
-			break;
-		case "F12":
-			value = (byte) 0x45;
-			break;
+        case "F1":
+            value = (byte) 0x3a;
+            break;
+        case "F2":
+            value = (byte) 0x3b;
+            break;
+        case "F3":
+            value = (byte) 0x3c;
+            break;
+        case "F4":
+            value = (byte) 0x3d;
+            break;
+        case "F5":
+            value = (byte) 0x3e;
+            break;
+        case "F6":
+            value = (byte) 0x3f;
+            break;
+        case "F7":
+            value = (byte) 0x40;
+            break;
+        case "F8":
+            value = (byte) 0x41;
+            break;
+        case "F9":
+            value = (byte) 0x42;
+            break;
+        case "F10":
+            value = (byte) 0x43;
+            break;
+        case "F11":
+            value = (byte) 0x44;
+            break;
+        case "F12":
+            value = (byte) 0x45;
+            break;
         }
 
         addByteToFile(value);
-	}
+    }
 
 	/**
 	 * Tests if the given string is a function key (for example, F1, F2, F3).
@@ -256,25 +256,25 @@ public class State {
 	 * 
 	 * @return boolean true indicates string is a function key, otherwise false
 	 */
-	public boolean isFunctionKey(String possibleFKey) {
-		switch (possibleFKey.toUpperCase()) {
-		case "F1":
-		case "F2":
-		case "F3":
-		case "F4":
-		case "F5":
-		case "F6":
-		case "F7":
-		case "F8":
-		case "F9":
-		case "F10":
-		case "F11":
-		case "F12":
-			return true;
-		default:
-			return false;
-		}
-	}
+    public boolean isFunctionKey(String possibleFKey) {
+        switch (possibleFKey.toUpperCase()) {
+        case "F1":
+        case "F2":
+        case "F3":
+        case "F4":
+        case "F5":
+        case "F6":
+        case "F7":
+        case "F8":
+        case "F9":
+        case "F10":
+        case "F11":
+        case "F12":
+            return true;
+        default:
+            return false;
+        }
+    }
     
 	/**
 	 * Starts state machine that parses input DuckyScript file. Reads file
@@ -284,27 +284,27 @@ public class State {
 	 */
     public void begin() {
         // Loop through file contents, line by line
-		for (int i = 0; i < instructions.length; i++) {
-			try {
-				String commentCheck = instructions[i].substring(0, 2);
+        for (int i = 0; i < instructions.length; i++) {
+            try {
+                String commentCheck = instructions[i].substring(0, 2);
 
                 // Ignore C++ style comments
-				if (commentCheck.equals("//"))
-					continue;
+                if (commentCheck.equals("//"))
+                    continue;
 
                 // Get command name from beginning of line
-				currInstruction = instructions[i].split(" ", 2);
-				currInstruction[0].trim();
+                currInstruction = instructions[i].split(" ", 2);
+                currInstruction[0].trim();
 
                 // Ignore excess command arguments
-				if (currInstruction.length == 2)
-					currInstruction[1].trim();
+                if (currInstruction.length == 2)
+                    currInstruction[1].trim();
 
                 // Find a match from the command list
                 for (int j = 0; j < commandList.size(); j++) {
                     Command cmd = commandList.get(j);
                     String  instr = currInstruction[0];
-                    System.out.println("cmd.name = " + cmd.getName());
+                    
                     if (instr.equals(cmd.getName())
                             || instr.equals(cmd.getAltName())) {
 
@@ -315,50 +315,49 @@ public class State {
 
                 // TODO Process REM somehow (maybe empty action() method)
 
-				if (isFunctionKey(currInstruction[0])) {
-					// Function keys
+                if (isFunctionKey(currInstruction[0])) {
+                    // Function keys
                     addFunctionKeyToFile(currInstruction[0]);
                     addByteToFile(0x00);
-				} else {
-					throw new Exception();
-				}
+                } else
+				    throw new Exception();
 
-				// Default delay between commands
-				if (!delayOverride & defaultDelay != 0x00) {
-					while (defaultDelay > 0) {
+                // Default delay between commands
+                if (!delayOverride & defaultDelay != 0x00) {
+                    while (defaultDelay > 0) {
                         addByteToFile(0x00);
 
-						if (defaultDelay > 255) {
+                        if (defaultDelay > 255) {
                             addByteToFile(0xFF);
-							defaultDelay = defaultDelay - 255;
-						} else {
+                            defaultDelay = defaultDelay - 255;
+                        } else {
                             addByteToFile(defaultDelay);
-							defaultDelay = 0;
-						}
-					}
-				}
-			} catch (Exception e) {
-				String errMsg = "[ERROR] Invalid command on line " + (i + 1);
-				System.err.println(errMsg);
-			}
-		}
+                            defaultDelay = 0;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                String errMsg = "[ERROR] Invalid command on line " + (i + 1);
+                System.err.println(errMsg);
+            }
+        }
 
-		// Write byte array to file
-		byte[] data = new byte[file.size()];
+        // Write byte array to file
+        byte[] data = new byte[file.size()];
 
-		for (int i = 0; i < file.size(); i++)
-			data[i] = file.get(i);
+        for (int i = 0; i < file.size(); i++)
+            data[i] = file.get(i);
 
-		try {
-			FileOutputStream fos = new FileOutputStream(new File(outFile));
+        try {
+            FileOutputStream fos = new FileOutputStream(new File(outFile));
 
-			fos.write(data);
-			fos.flush();
-			fos.close();
-		} catch (FileNotFoundException e) {
-			System.err.print("[ERROR] Failed to create output file: " + outFile);
+            fos.write(data);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            System.err.print("[ERROR] Failed to create output file: " + outFile);
         } catch (Exception e) {
-			System.err.print("[ERROR] Failed to write output file: " + outFile);
-		}
+            System.err.print("[ERROR] Failed to write output file: " + outFile);
+        }
     }
 }
