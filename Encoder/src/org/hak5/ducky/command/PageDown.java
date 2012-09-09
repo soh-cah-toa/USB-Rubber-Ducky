030786bcad3d194fc160accace73073ad72d02fd
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 14:35:51 -0400

    Implemented PAGEDOWN command.
    PAGEDOWN command is now represented by PageDown class.

---
+++ Encoder/src/org/hak5/ducky/command/PageDown.java
@@ -0,0 +1,37 @@
+package org.hak5.ducky.command;
+
+import org.hak5.ducky.State;
+import org.hak5.ducky.Command;
+
+/**
+ * @author Kevin Polulak
+ *
+ * Implementation of the <code>PAGEDOWN</code> command. Represents the PgDn key
+ * used to scroll down in documents.
+ */
+public class PageDown implements Command {
+    public final java.lang.String name    = "PAGEDOWN";
+    public final java.lang.String altName = "";
+    public final java.lang.String help    = name;
+
+    @Override
+    public void action(State state, java.lang.String args) {
+        state.addByteToFile(0x4E);
+        state.addByteToFile(0x00);
+    }
+    
+    @Override
+    public java.lang.String getName() {
+        return name;
+    }
+    
+    @Override
+    public java.lang.String getAltName() {
+        return altName;
+    }
+    
+    @Override
+    public java.lang.String getHelp() {
+        return help;
+    }
+}
