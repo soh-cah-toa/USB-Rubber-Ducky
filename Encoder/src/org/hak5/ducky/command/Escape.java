fd7bd4be8a43ececd973e58d6aec1d2ac58adfa0
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 14:29:57 -0400

    Introduced several new Command classes.
    New classes include Delay, Delete, DownArrow, End, Enter, Escape, Home,
    Insert, LeftArrow, and Menu.

---
+++ Encoder/src/org/hak5/ducky/command/Escape.java
@@ -0,0 +1,37 @@
+package org.hak5.ducky.command;
+
+import org.hak5.ducky.State;
+import org.hak5.ducky.Command;
+
+/**
+ * @author Kevin Polulak
+ *
+ * Implementation of the <code>ESCAPE/ESC</code> command. Represents the Esc
+ * key used to initiate an escape sequence.
+ */
+public class Escape implements Command {
+    public final java.lang.String name    = "ESCAPE";
+    public final java.lang.String altName = "ESC";
+    public final java.lang.String help    = name + " | " + altName;
+
+    @Override
+    public void action(State state, java.lang.String args) {
+        state.addByteToFile(0x29);
+        state.addByteToFile(0x00);
+    }
+    
+    @Override
+    public java.lang.String getName() {
+        return name;
+    }
+    
+    @Override
+    public java.lang.String getAltName() {
+        return altName;
+    }
+    
+    @Override
+    public java.lang.String getHelp() {
+        return help;
+    }
+}
