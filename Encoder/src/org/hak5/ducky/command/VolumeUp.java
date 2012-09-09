0d873ed3affa1097c08ad1a680c66551a917bdc1
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 17:02:52 -0400

    Implemented VOLUMEUP command.
    VOLUMEUP command is now represented by VolumeUp class.

---
+++ Encoder/src/org/hak5/ducky/command/VolumeUp.java
@@ -0,0 +1,37 @@
+package org.hak5.ducky.command;
+
+import org.hak5.ducky.State;
+import org.hak5.ducky.Command;
+
+/**
+ * @author Kevin Polulak
+ *
+ * Implementation of the <code>VOLUMEUP</code> command. Represents the Volume
+ * Up key present on some keyboards used to turn the speaker volume up.
+ */
+public class VolumeUp implements Command {
+    public final java.lang.String name    = "VOLUMEUP";
+    public final java.lang.String altName = "";
+    public final java.lang.String help    = name;
+
+    @Override
+    public void action(State state, java.lang.String args) {
+        state.addByteToFile(0xE9);
+        state.addByteToFile(0x00);
+    }
+    
+    @Override
+    public java.lang.String getName() {
+        return name;
+    }
+    
+    @Override
+    public java.lang.String getAltName() {
+        return altName;
+    }
+    
+    @Override
+    public java.lang.String getHelp() {
+        return help;
+    }
+}
