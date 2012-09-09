9cfda04614ba375e9932113c9a1f8b0db735cfd9
Kevin Polulak <kpolulak@gmail.com>
2012-09-09 14:40:10 -0400

    Implemented RIGHTARROW/RIGHT command.
    RIGHTARROW/RIGHT command is now represented by RightArrow class.

---
+++ Encoder/src/org/hak5/ducky/command/RightArrow.java
@@ -0,0 +1,37 @@
+package org.hak5.ducky.command;
+
+import org.hak5.ducky.State;
+import org.hak5.ducky.Command;
+
+/**
+ * @author Kevin Polulak
+ *
+ * Implementation of the <code>RIGHTARROW/RIGHT</code> command. Represents the
+ * Right Arrow key used to move the cursor to the right.
+ */
+public class RightArrow implements Command {
+    public final java.lang.String name    = "RIGHTARROW";
+    public final java.lang.String altName = "RIGHT";
+    public final java.lang.String help    = name + " | " + altName;
+
+    @Override
+    public void action(State state, java.lang.String args) {
+        state.addByteToFile(0x4F);
+        state.addByteToFile(0x00);
+    }
+    
+    @Override
+    public java.lang.String getName() {
+        return name;
+    }
+    
+    @Override
+    public java.lang.String getAltName() {
+        return altName;
+    }
+    
+    @Override
+    public java.lang.String getHelp() {
+        return help;
+    }
+}
